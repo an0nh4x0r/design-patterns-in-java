8dc673e5e65e39ecb9e66d9a2e1311c9de387319
swapnil singh <swappp7singh@gmail.com>
2019-01-02 21:43:18 +0530

    Builder design pattern example

---
+++ Creational/src/main/java/in/swapnilsingh/builder/BuilderEverydayDemo.java
@@ -0,0 +1,16 @@
+package in.swapnilsingh.builder;
+
+public class BuilderEverydayDemo {
+
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("This is an example ");
+        builder.append("of the builder pattern. ");
+        builder.append("It has method to append ");
+        builder.append("almost anything we want to a String. ");
+        builder.append(42);
+
+        System.out.println(builder.toString());
+    }
+
+}
---
+++ Creational/src/main/java/in/swapnilsingh/builder/LunchOrderTeleDemo.java
@@ -0,0 +1,13 @@
+package in.swapnilsingh.builder;
+
+public class LunchOrderTeleDemo {
+    public static void main(String[] args) {
+        LunchOrderTele lunchOrderTele = new LunchOrderTele("Wheat", "Lettuce",
+                "Mustard", "Ham");
+
+        System.out.println(lunchOrderTele.getBread());
+        System.out.println(lunchOrderTele.getCondiments());
+        System.out.println(lunchOrderTele.getDressing());
+        System.out.println(lunchOrderTele.getMeat());
+    }
+}
